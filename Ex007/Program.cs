8a1bd0352abf18c72383d9f3f61b00a6f4fb1a0e
Alexsandro <alexsandro.ljesus@senacsp.edu.br>
2025-01-16 22:26:51 -0300

    Exerício8

--- Ex007/Program.cs
+++ Ex007/Program.cs
@@ -8,18 +8,46 @@
 
 
 
-            double nota_final = 70;
-            string resultado = "Reprovado";
+            double nota_final, nota1, nota2, nota3, nota4;
+            Console.WriteLine("Digite 1ª nota do aluno: ");
+            nota1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite 2ª nota do aluno: ");
+            nota2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite 3ª nota do aluno: ");
+            nota3 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite 4ª nota do aluno: ");
+            nota4 = Convert.ToDouble(Console.ReadLine());
+
+            nota_final = (nota1 + nota2 + nota3 + nota4) / 4;
+
+
+            string resultado;
 
             if (nota_final >= 60)
             {
-                resultado = "Aprovado";
-                Console.WriteLine("O aluno foi {0}.", resultado);
+                if (nota_final >= 90)
+                {
+                    resultado = "Aprovado com louvor";
+                }
+                else
+                {
+                    resultado = "Aprovado";
+                }
+            }
+            if (nota_final >= 40)
+            {
+                resultado = "Recuperação";
             }
             else
             {
-                Console.WriteLine("O aluno foi {0}.", resultado);
+                resultado = "Reprovado";
             }
+
+            Console.WriteLine("O nota do aluno: {0} - Resultado: {1}.", nota_final, resultado);
+
 
 
 
@@ -27,5 +55,4 @@
         }
     }
 }
-
 
---
+++ Ex008/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Ex008
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+            //Para saber se o numero é par ou impar é só fazer um if else e dividir o numero por 2. Se o modulo da operação for igual a zero é par, senão é impar
+            Console.WriteLine("Informe um número");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("O numero é par");
+            }
+            else
+            {
+                Console.WriteLine("O número é impar");
+            }
+
+
+        }
+    }
+}
