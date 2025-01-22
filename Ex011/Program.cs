d0e5f9de72364d7ee28e433a3f6d4fca5f03348c
Alexsandro <alexsandro.ljesus@senacsp.edu.br>
2025-01-21 22:06:59 -0300

    Ex010

---
+++ Ex011/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Ex011
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Qual o valor da casa?");
+            double valorCasa = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Qual o seu salário?");
+            double seuSal = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Quantos anos para pagar?");
+            int anos = Convert.ToInt32(Console.ReadLine());
+
+            int meses = anos * 12;
+            double presMensal = valorCasa / meses;
+            double limitePres = seuSal * 0.3;
+
+            if (presMensal > limitePres)
+        
+            {
+                Console.WriteLine("Empréstimo negado");
+            }
+            else { Console.WriteLine("Empréstimo aceito"); 
+            }
+
+            
+        
+
+                    
+        }
+    }
+}
